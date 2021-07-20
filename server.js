06eec7cc1c51119601ba7a901008b7cc36715c87
Cam <cam.ball96@hotmail.com>
2021-07-20 21:54:35 +1000

    added server.js file start js routes

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+const db = require("./develop/db/db.json")
+const util = require("util");
+const PORT = process.env.PORT || 3000;
+
+
+//const readFileAs = util.promisify(fs.readFile);
+//const writeFileAs = util.promisify(fs.writeFile);
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("./develop/public"));
+
+
+
+//HTML Routes
+app.get('/notes' , function (req, res) {
+    res.sendFile(path.join(__dirname, './develop/public/notes.html'));
+});
+app.get('/' , function (req, res) {
+    res.sendFile(path.join(__dirname, './develop/public/index.html'));
+});
+app.get('*', function (req,res){
+    res.sendFile(path.join(__dirname, './develop/public/index.html'));
+});
+
+
+//API Routes
+
+/*app.get("/api/notes", (req,res) => {
+    readFileAs("./develop/db/db.json", "utf8")
+    .then(function(data){
+        notes = [].concat(JSON.parse(data))
+        res.json(notes);
+    });
+}); */
+
+app.get("/api/notes", (req,res) => {
+    let notesJ = fs.readFileSync("./develop/db/db.json");
+    let note = JSON.parse(notesJ);
+    res.send(note.slice(1));
+})
+
+
+
+
+app.listen(PORT, function() {
+    console.log("app is listening on PORT: 3000");
+});
